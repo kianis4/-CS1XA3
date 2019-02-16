95e227ab940b948b1399ddcfc35bbf1ec840ec10
kianis4 <kianis4@mcmaster.ca>
2019-02-16 04:44:09 +0000

    Fixed file type count feature

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -2,7 +2,8 @@
 erin=0
 while [ erin=1 ]
 do
-read -r -p "What number Feature would you like to use?\n1.Delete Temp File?\n2.File Type count?" featName
+echo -e "What number Feature would you like to use?\n1.Delete Temp File?\n2.File Type Count?"
+read -r featName
 
         if [ $featName == 2 ]; then
 	    erin=0
@@ -13,12 +14,12 @@ read -r -p "What number Feature would you like to use?\n1.Delete Temp File?\n2.F
 	    haskellFind=$(find ~/CS1XA3/ -name "*.hs" | wc -l)
 	    bashFind=$(find ~/CS1XA3/ -name "*.sh" | wc -l)
 
-	    echo HTML  : $htmlFile
-	    echo JAVA  : $javaFile
-	    echo CSS   : $cssFile
-	    echo Python: $pyFile
-	    echo HASKEL: $haskellFile
-	    echo BASH  : $bashFile
+	    echo HTML  : $htmlFind
+	    echo JAVA  : $javaFind
+	    echo CSS   : $cssFind
+	    echo Python: $pyFind
+	    echo HASKEL: $haskellFind
+	    echo BASH  : $bashFind
 
         elif [ $featName == 1 ]; then
             echo this is feat 1
