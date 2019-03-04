edaa851569231c91eaf81de48f16d8d6ca4c5d42
kianis4 <kianis4@mcmaster.ca>
2019-03-04 14:52:26 +0000

    Comiled elm file

--- Lab06/src/Main.elm
+++ Lab06/src/Main.elm
@@ -11,20 +11,18 @@ type alias Model =
 
 init : Model
 
-init = { string1 = "", string2 = ""}
+init = { str1 = "", str2 = ""}
 
 view : Model -> Html Msg
-view model = div []
-
-    [ input [ placeholder "String 1", value model.str1, onInput (Update 1) ] []
-    , input [ placeholder "String 2", value model.str2, onInput (Update 2) ] []
-    , div [] [ text (model.str1 ++ " : " ++ model.str2 ) ]
-    ]
+view model = div [] [
+      input [ placeholder "String 1", value model.str1, onInput ChangeFirst  ] []
+    , input [ placeholder "String 2", value model.str2, onInput ChangeSecond ] []
+    , div [] [text (model.str1 ++ " : " ++ model.str2)] ]
 
 update : Msg -> Model -> Model
 update msg model = case msg of
-                    Change1 newContent -> {model | str1 = newContent}
-                    Change2 newContent -> {model | str2 = newContent}
+                    ChangeFirst newContent -> {model | str1 = newContent}
+                    ChangeSecond newContent -> {model | str2 = newContent}
 
 type Msg = ChangeFirst String
             | ChangeSecond String
